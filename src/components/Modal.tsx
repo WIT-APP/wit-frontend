b9e50ecac37e9df4a99a8465a6485c265a094e8e
Angela-GM <angelagm0112@gmail.com>
2023-09-21 10:56:33 +0200

    #3 update pre merge

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,13 +12,19 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { IoLogoWhatsapp } from "react-icons/io";
 
-
 type ModalProps = Partial<Applicant> & {
   textButton: string | React.ReactNode;
 };
 
-function Modal({ textButton, nombre, programa_cursar, apellidos, telefono }: ModalProps) {
-    const [open, setOpen] = useState(false);
+function Modal({
+  textButton,
+  nombre,
+  programa_cursar,
+  apellidos,
+  telefono,
+}: ModalProps) {
+  const [open, setOpen] = useState(false);
+//   Refactorizar esta parte??
   const [mensaje, setMensaje] = useState(
     `Hola ${nombre}! Soy Daniela, de Somos F5. Te escribo para recordarte que mañana nos vemos en la Sesión de Selección para el BOOTCAMP DE ${programa_cursar?.toUpperCase()} :) La sesión empieza a las 15:30h (hora peninsular). Hablaremos del programa y haremos una entrevista personal. Por favor, confírmame si estarás. La asistencia es obligatoria para entrar en el bootcamp. Aquí te dejo el enlace de Zoom: https://us06web.zoom.us/j/6368867811 Un saludo, Dani`
   );
@@ -29,21 +35,21 @@ function Modal({ textButton, nombre, programa_cursar, apellidos, telefono }: Mod
     setMensaje(event.target.value);
   };
 
-  const handleSendMessage = ( telefono: string | undefined , mensaje:string ) => {
-    const whatsappURL = `https://web.whatsapp.com/send?phone=34${telefono}&text=${encodeURIComponent(mensaje)}`;
-    
-    window.open(whatsappURL, '_blank');
-    
+  const handleSendMessage = (telefono: string | undefined, mensaje: string) => {
+    const whatsappURL = `https://web.whatsapp.com/send?phone=34${telefono}&text=${encodeURIComponent(
+      mensaje
+    )}`;
 
-    // añadir funcionalidad para enviar el mensaje por whatsapp http://web.whatsapp.com/send?phone=34{telefono}&text={mensaje}
+    window.open(whatsappURL, "_blank");
+
+    // Añadir funcionalidad para cambiar estado en la base de datos del campo estado a Invitado
 
     alert(mensaje + telefono);
-    setOpen(false);    
-    
-  }
+    setOpen(false);
+  };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen} >
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>{textButton}</DialogTrigger>
       <DialogContent className="max-h-[80vh]">
         <DialogHeader>
@@ -59,10 +65,11 @@ function Modal({ textButton, nombre, programa_cursar, apellidos, telefono }: Mod
             />
           </DialogDescription>
         </DialogHeader>
-        <Button variant="ghost" >
-          Atras
-        </Button>
-        <Button onClick={() => handleSendMessage( telefono , mensaje)} className="bg-green hover:bg-yellow">
+        <Button variant="ghost">Atras</Button>
+        <Button
+          onClick={() => handleSendMessage(telefono, mensaje)}
+          className="bg-green hover:bg-yellow"
+        >
           <IoLogoWhatsapp className="mr-2 h-4 w-4" /> Enviar Mensaje
         </Button>
       </DialogContent>
