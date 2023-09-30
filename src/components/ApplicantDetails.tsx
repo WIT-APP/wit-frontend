30d7573270657fc6804fce106d6a17a651be20c4
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-09-30 14:10:09 +0200

    success modal for saving changes added

--- src/components/ApplicantDetails.tsx
+++ src/components/ApplicantDetails.tsx
@@ -15,8 +15,9 @@ import { useUpdateApplicant } from "@/services/UpdateApplicant";
 import labelsArray from "../data/interview-labels.data.json";
 import { useNavigate, useParams } from "react-router-dom";
 import { Applicant } from "@/interfaces/applicant.interface";
-import { UnsavedChangesConfirmationDialog } from "./UnsavedChangesDialog";
+import { UnsavedChangesConfirmationDialog } from "../alerts/UnsavedChangesDialog";
 import { Textarea } from "./ui/textarea";
+import { ChangesSavedDialog } from "@/alerts/ChangesSavedDialog";
 
 export const ApplicantDetails = () => {
   const { id } = useParams();
@@ -30,6 +31,7 @@ export const ApplicantDetails = () => {
   const [applicantInfo, setApplicantInfo] = useState<Applicant | null>(null);
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [unsavedChanges, setUnsavedChanges] = useState(false);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
   useEffect(() => {
     if (!isLoading && !isError && oneApplicant) {
@@ -79,11 +81,17 @@ export const ApplicantDetails = () => {
         });
         queryClient.invalidateQueries(["applicants", applicantId]);
         console.log("Applicant information updated successfully.");
+        setShowSuccessModal(true);
       } catch (error) {
         console.error("Error updating applicant information:", error);
       }
     }
   };
+
+  const handleSuccessModalClose = () => {
+    setShowSuccessModal(false);
+  };
+
   const navigate = useNavigate();
 
   const handleCancel = () => {
@@ -110,10 +118,10 @@ export const ApplicantDetails = () => {
           Mostrar y modificar la información del aspirante.
         </CardDescription>
       </CardHeader>
-      <CardContent className="grid py-2 gap-6 max-h-[72vh] lg:max-h-[80vh] overflow-y-auto">
+      <CardContent className="grid py-2 gap-6 max-h-[75vh] md:max-h-[80vh] overflow-y-auto">
         {Array.from(new Set(labelsArray.map((item) => item.category))).map((category) => (
           <div key={category}>
-            <h2 className="text-xl font-bold mb-2 p-1 bg-green2">{category}</h2>
+            <h2 className="text-xl font-bold mb-2 p-1 bg-green2 ">{category}</h2>
             <div
               className={`grid gap-2  ${
                 category === "Notas" ? "grid-cols-1" :
@@ -149,6 +157,7 @@ export const ApplicantDetails = () => {
           onClose={() => setShowConfirmation(false)} 
           onContinue={handleConfirmationContinue}
         />
+        <ChangesSavedDialog isOpen={showSuccessModal} onClose={handleSuccessModalClose}  />
           <Button className="btn-form-green" onClick={handleCancel}>Cancelar</Button>
         <Button className="btn-form-green" onClick={handleSubmit}>Enviar</Button>
       </CardFooter>
