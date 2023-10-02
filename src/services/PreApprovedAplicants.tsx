32e6c00f9e10143817210c66601d4df6af7bef52
Angela-GM <angelagm0112@gmail.com>
2023-10-02 22:01:40 +0200

    #4 arreglar errores de PreApprovedAplicants

--- src/services/PreApprovedAplicants.tsx
+++ src/services/PreApprovedAplicants.tsx
@@ -2,36 +2,34 @@ import { useQuery } from "@tanstack/react-query";
 import { Applicant } from "../interfaces/applicant.interface";
 import { format } from "date-fns";
 
-
 export const usePreApprovedApplicants = () => {
   const {
     isLoading,
     isError,
     data: applicant,
   } = useQuery({
-    queryKey: ['applicants'],
+    queryKey: ["applicants"],
     queryFn: async (): Promise<Applicant[]> => {
       const response = await fetch(
-      //  'https://wit-backend-factoriaf5.up.railway.app/applicant/preapproved-applicants'
-      // 'http://localhost:3000/applicant/preapproved-applicants'
-      'https://wit-backend-factoriaf5.up.railway.app/applicant/preapproved-applicants'
+        //  'https://wit-backend-factoriaf5.up.railway.app/applicant/preapproved-applicants'
+        // 'http://localhost:3000/applicant/preapproved-applicants'
+        "https://wit-backend-factoriaf5.up.railway.app/applicant/preapproved-applicants"
       );
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       // Formatea la fecha de nacimiento en cada solicitante
-    const formattedApplicants = data.map((applicant: Applicant) => ({
-      ...applicant,
-      fecha_de_applicacion: format(new Date(applicant.fecha_de_applicacion), "dd/MM/yyyy"),
-      fecha_de_nacimiento: format(new Date(applicant.fecha_de_nacimiento), "dd/MM/yyyy"),
-      telefono: applicant.telefono.toString()
-    }));
+      const formattedApplicants = data.map((applicant: Applicant) => ({
+        ...applicant,
+        fecha_de_applicacion: applicant.fecha_de_applicacion
+          ? format(new Date(applicant.fecha_de_applicacion), "dd/MM/yyyy")
+          : "",
+        fecha_de_nacimiento: applicant.fecha_de_nacimiento
+          ? format(new Date(applicant.fecha_de_nacimiento), "dd/MM/yyyy")
+          : "",
+        telefono: applicant.telefono ? applicant.telefono.toString() : "",
+      }));
 
-    return formattedApplicants;
-
-
-    
-    
-      // return data;
+      return formattedApplicants;
     },
     staleTime: 50000000,
   });
